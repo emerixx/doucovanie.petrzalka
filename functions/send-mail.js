e18961b5102bf403e7035a0569085e5bb0b8904c
emerixx <juraj.svatek@gmail.com>
2025-01-14 20:38:31 +0100

    idk0

--- functions/send-mail.js
+++ functions/send-mail.js
@@ -9,6 +9,9 @@
  */
 
 
+export function onRequest(context) {
+    return new Response("Hello, world!")
+}
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
