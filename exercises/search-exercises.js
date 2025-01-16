8622fd167e5d772485788013de37fa8911b1a2b1
emerixx <juraj.svatek@gmail.com>
2025-01-16 12:09:57 +0100

    idek atp

--- exercises/search-exercises.js
+++ exercises/search-exercises.js
@@ -1,2 +1,6 @@
 let search_bar = document.getElementById("exercises_nav_search_bar");
-search_bar.onchange = console.log(search_bar.innerText);
+search_bar.addEventListener('change', function() {
+  console.log('Search input changed to:', this.value);
+  // Call your function here
+  
+});
